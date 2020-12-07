8aa58a8bd71673c342b5908663f33b253b849cf6
thmantunes@gmail.com <andromeda2192>
2020-12-07 01:56:18 -0300

    first commit

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -26,6 +26,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -55,24 +56,38 @@ public class ChromeTest {
     @After
     public void teardown() {
         if (driver != null) {
-            driver.quit();
+          //  driver.quit();
         }
     }
 
     @Test
     public void test() {
-        // Your test code here. For example:
         WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.get("https://en.wikipedia.org/wiki/Main_Page");
         By searchInput = By.id("searchInput");
         wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
+        driver.findElement(searchInput).sendKeys("Hk416");
         By searchButton = By.id("searchButton");
         wait.until(elementToBeClickable(searchButton));
         driver.findElement(searchButton).click();
 
-        wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+      //
+        wait.until(textToBePresentInElementLocated(By.tagName("body"), "Heckler & Koch"));
+        driver.findElement(By.linkText("Heckler & Koch")).click();
+
+        driver.findElement(By.xpath("//a[@href='http://www.heckler-koch.de/']")).click();
+
+
+
+
+
+
+//www.heckler-koch.de
+
     }
+
+
+
+
 
 }
